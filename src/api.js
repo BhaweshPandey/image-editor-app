c06670b993d818bb2c17c0cdb97af9c8e816782e
Bhawesh Pandey <111113334+BhaweshPandey@users.noreply.github.com>
2025-01-17 05:26:58 +0000

    Default API key

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://pixabay.com/api/",
   params: {
-    key: "48265745-0451a98a57d203525a8722ba8", // Default API key
+    key: "48265745-0451abhhnnnnn98a57d203525a8722ba8", // Default API key
   },
 });
 
